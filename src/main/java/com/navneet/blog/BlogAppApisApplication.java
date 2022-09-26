4591da90fa0ddbd93460a3090a15d961f13eb1c7
navneet <navneet@192.168.1.2>
2022-09-26 09:21:19 +0530

    first-commit

---
+++ src/main/java/com/navneet/blog/BlogAppApisApplication.java
@@ -0,0 +1,14 @@
+package com.navneet.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppApisApplication {
+
+	//Main file
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppApisApplication.class, args);
+	}
+
+}
